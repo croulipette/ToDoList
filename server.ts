0e707f5002be449f3d8069eab9d497c9ec684ab1
Patrice Vaquant <p.vaquant@lectra.com>
2018-01-02 09:04:05 +0100

    Refacto Server.ts en classe

--- server.ts
+++ server.ts
@@ -4,44 +4,48 @@ import Router from './src/Router';
 import * as express from 'express';
 import * as http from 'http';
 
-// //routes = require('./src/routes/index'),
-// //HttpStatus = require('http-status-codes'),
+class Server {
 
+    public app: express.Application;
+    public server: http.Server;
 
+    constructor() {
+        //create expressjs application
+        this.app = express();
+        this.configAppSettings();
+        this.createServer()
+    }
+    public static start(): Server {
+        return new Server();
+    }
 
-// // Init routes
-// //routes(app);
+    private configAppSettings() {
+        this.app.set('port', process.env.port || 3001);
+    }
+    private createServer(): any {
+        this.server = http.createServer(this.app);
+        this.server.listen(this.app.get('port'));
+        this.server.on('listening', this.onListening);
+    }
 
-
-
-
-const app = express();
-// APP Config
-
-app.set('port', process.env.port || 3001);
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app);
-server.listen(app.get('port'));
-//server.on('error', onError);
-server.on('listening', onListening);
-
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    console.log('Server started Listening on ' + bind);
+    /**
+     * Event listener for HTTP server "listening" event.
+     */
+    private onListening() {
+        const addr = this.server.address();
+        const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+        console.log('Server started Listening on ' + bind);
+    }
 }
 
 
 
+
+const server = Server.start();
+export default server.app;
+
 // //init route
-new Router(app).initRoute();
+//new Router(app).initRoute();
 //app.get('/', function (req: any, res: any) { res.send('hello world'); })
+
 
